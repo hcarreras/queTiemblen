11e1af73a385561baf58163f3ee8a497646b68f3
Hari Carreras <hcarreras@MacBook-Pro-15-de-Hari-Carreras.local>
2012-08-10 20:37:25 +0200

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quetiemblen::Application.config.secret_token = '364e2939b7d2a0619e814f82aecb20da4643d444af4fa3b47000f831e8abe725d271b5045a6636371c4b6c1a0b9c7a395944e0c0b35745618bec8f451fed7779'
