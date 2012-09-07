700b7dfc002841166594c06ccc1b4d9948af761d
Hari Carreras <haricarreras@gmail.com>
2012-09-07 14:11:03 +0200

    Same product different brands

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ActiveRecord::Base
   attr_accessible :brand_id, :title
   belongs_to :brand
 
-  validates :title, :presence => true, :uniqueness => { :message => 'The product already exists'}
+  validates :title, :presence => true, :uniqueness => { :scope => :brand_id, :case_sensitive => false }
   validates :brand, :presence => {:message => 'The brand no exists'}
-
-
 end
