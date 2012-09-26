942e7bba31791aa0123fcb047d6ed63bbebd1f7c
Hari Carreras <haricarreras@gmail.com>
2012-09-26 16:57:58 +0200

    Validates comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :brand
   belongs_to :product
   belongs_to :brand
+
+  validates :brand, :presence => {:unless => :product_id?, :message => "The comment must belongs to a brand or product"}
+  validates :product, :presence => {:unless => :brand_id?, :message => "The comment must belongs to a brand or product"}
 end
